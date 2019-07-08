06c204fc99271fb88265c348164842c1bffd5a6c
Yihiu Chiu <adamc@YihiudeMBP.andrew.cmu.edu>
2019-07-08 11:47:08 -0400

    developed basic UI for detection on live page

--- main/static/main/js/live.js
+++ main/static/main/js/live.js
@@ -1,8 +1,11 @@
+var initial_call = true;
+var last_entry = 0;
+
 $(document).ready(function () {
     videoOutput = $("#videoOutput");
     setupPage();
     start();
-    $("#videoOutput").resize(resetVideoSize);
+    videoOutput.resize(resetVideoSize);
     $(window).resize(resetVideoSize);
 
 });
@@ -35,6 +38,41 @@ function resetVideoSize() {
     var video = $('#videoOutput');
     video_width = video.width();
     video_height = video.height();
+}
+
+function get_detection_update(num_entries) {
+    $.ajax({
+        url: "/api/detection",
+        dataType: "json",
+        type: 'GET',
+        data: {
+            num_entries: num_entries,
+            initial_call: initial_call,
+            last_entry: last_entry
+        },
+        context: document.body,
+        success: function (data) {
+            initial_call = false;
+            if (data.detection_entry.length > 0) {
+                last_entry = data.detection_entry[0].detection_id;
+            }
+            var row_content = "";
+            $.each(data.detection_entry, function (k, v) {
+                row_content += "<tr><td>"
+                    + v.detect_time + "</td><td>"
+                    + v.visitor_id + "</td><td>"
+                    + v.name + "</td><td>"
+                    + v.location + "</td><td><img class='live_thumbnail ' src='/media/photos/"
+                    + v.detect_pic + "'/></td><td><img class='live_thumbnail' src='/media/photos/"
+                    + v.regist_pic + "'/></td></tr>";
+
+            });
+            $("#detection_table tbody").prepend(row_content);
+        },
+        error: function (data) {
+            console.log("Service error, please contact admin.")
+        }
+    });
 }
 
 String.prototype.format = String.prototype.f = function () {
