857e9d2bb8a719bc6f8696b83fd16164cf4b0782
Yihiu Chiu <adamc@YihiudeMBP.andrew.cmu.edu>
2019-07-12 14:23:37 -0400

    validate age to be number only

--- main/static/main/js/enrollment.js
+++ main/static/main/js/enrollment.js
@@ -31,14 +31,17 @@ function submitform() {
 }
 
 function validate() {
-    if ($("[name=img_holder]").val().trim().length == 0) {
+    if ($("[name=img_holder]").val().trim().length === 0) {
         $("#message").html("Please upload a image.");
         return false;
-    } else if ($("[name=fname]").val().trim().length == 0) {
+    } else if ($("[name=fname]").val().trim().length === 0) {
         $("#message").html("Please enter your first name.");
         return false;
-    } else if ($("[name=lname]").val().trim().length == 0) {
+    } else if ($("[name=lname]").val().trim().length === 0) {
         $("#message").html("Please enter your last name.");
+        return false;
+    } else if ($("[name=age]").val().trim().length !== 0 && isNaN($("[name=age]"))) {
+        $("#message").html("Age should be numbers only.");
         return false;
     } else {
         return true;
