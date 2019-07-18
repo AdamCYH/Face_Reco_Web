666717ed2d3a81937a3c5dfcb977639a7fff8913
Yihiu Chiu <adamc@YihiudeMBP.andrew.cmu.edu>
2019-07-18 10:44:02 -0400

    handles the case when use in sdk memory but not in database

--- main/utilities/sql.py
+++ main/utilities/sql.py
@@ -1,6 +1,10 @@
+import logging
+
 from django.utils import timezone
 
 from main.models import User, MatchJob, MatchUser
+
+logger = logging.getLogger(__name__)
 
 
 def insert_visitor(fname, lname, age, description, photo_path):
@@ -26,10 +30,14 @@ def create_job():
 def insert_match_result(match_result):
     match_job = MatchJob.objects.get(job_id=match_result["job_id"])
     for user in match_result["users"]:
-        match_user = MatchUser()
-        match_user.job = match_job
-        match_user.user = User.objects.get(user_id=user["user_id"])
-        match_user.confidence_level = user["confidence_level"]
-        match_user.save()
+        try:
+            match_user = MatchUser()
+            match_user.job = match_job
+            match_user.user = User.objects.get(user_id=user["user_id"])
+            match_user.confidence_level = user["confidence_level"]
+            match_user.save()
+        except User.DoesNotExist:
+            logger.error(":::::[Warning] Matched user {} no longer in the database.".format(user["user_id"]))
+            continue
 
     return match_job
